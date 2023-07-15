5a26b362b121e7ee56d88eda51f0e785ceb48ddc
Rudra1306 <rudraparikh6@gmail.com>
2023-07-15 20:43:18 +1000

    Initial Commit

---
+++ Iphone_calculator/Iphone_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iphone_calculatorApp.swift
+//  Iphone_calculator
+//
+//  Created by Rudra on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Iphone_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
